3f58666d530eb3074490148db0eb968b1e8dffd3
maleke <malekelthomas97@gmail.com>
2020-09-30 18:15:28 -0400

    Updated to fill div and product stored category in input val

--- resources/views/choose-products.blade.php
+++ resources/views/choose-products.blade.php
@@ -17,18 +17,21 @@
         <div class="category-ellipses col-2">
 
           <?php
-            $i = 0;
-            foreach($_SESSION["images"] as $categories => $products){
-                foreach($products as $details){
-                  //details - [product-url, product-img]
-                    $links = preg_split("/[,]/", $details);?>
-                    <div style="vertical-align:middle" id="ellipse<?php echo $i;?>" class="ellipse">
-                        <?php echo "<img style = 'min-width:100%; min-height:100%;' value ='{$links[0]}' src={$links[1]}>"; ?>
-                        <input type="hidden" name="products[]" value='<?php echo "{$links[0]} {$links[1]}"?>'/>
-              </div><?php
-                $i++;
-                }
-            }
+            if (!empty($_SESSION["images"])){
+              $shuffledImages = $_SESSION["images"];
+              $i = 0;
+              foreach($shuffledImages as $categories => $products){
+                  foreach($products as $details){
+                    //details - [product-url, product-img]
+                      $links = preg_split("/[,]/", $details);?>
+                      <div style="vertical-align:middle; background-size:cover; background-position: center; background-image:url(<?php echo "{$links[1]}";?>); background-repeat:no-repeat;" id="ellipse<?php echo $i;?>" class="ellipse">
+                          <?php // echo "<img style = 'min-width:100%; min-height:100%;' value ='{$links[0]}' src={$links[1]}>"; ?>
+                          <input type="hidden" name="products[]" value='<?php echo "{$categories}";//{$links[0]} {$links[1]}"?>'/>
+                </div><?php
+                  $i++;
+                  }
+              }
+          }
           ?>
           
         </div>
@@ -40,12 +43,12 @@
     <div class="w-100" style="height: 100px;"></div>
     <div class="row justify-content-center">
       <div>
-        <p class="choose-cats">Choose Categories</p>
+        <p class="choose-cats">Choose Products</p>
       </div>
     </div>
     <div class="w-100" style="height: 100px;"></div>
     <div class="row justify-content-center">
-      <button class="next-button-button" onclick=submitCategories(event);>NEXT</button>
+      <button class="next-button-button" onclick=submitProducts(event);>NEXT</button>
     </div>
 
 
