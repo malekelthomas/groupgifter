550740e050e59ef951a686cacc06b8e636061320
maleke <malekelthomas97@gmail.com>
2020-10-01 01:45:07 -0400

    Categories of products picked and num clicked saves to db

--- resources/views/selected-products.php
+++ resources/views/selected-products.php
@@ -3,10 +3,25 @@
 
 <?php
 
+use Illuminate\Support\Facades\Auth;
+
+//get the currently authenticated user
+$user = Auth::user();
+
+// Get the currently authenticated user's ID...
+
+$id = Auth::id();
+
 $post = $_POST["products"];
 
 foreach($post as $product => $categories){
-    echo "$product,$post[$product]\n";
+    $entry = new App\Models\PickedCategory();
+    $entry->user_id = $id;
+    $entry->category= $product;
+    $entry->num_picked = $post[$product];
+    $entry->save();
+    echo "$entry\n";
+
     }
 
 
