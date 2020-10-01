fdf9558926dd67476a5f2888e2c3cd4412e3dc62
maleke <malekelthomas97@gmail.com>
2020-10-01 13:46:39 -0400

    Added group_name into registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'group_name' => ['alpha_dash', 'max:20', 'min:10'],
         ]);
     }
 
@@ -66,6 +67,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'group_name' => $data['group_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
