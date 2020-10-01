b0d0f32d8a8356140c8d003edadcf70b906363d6
maleke <malekelthomas97@gmail.com>
2020-10-01 01:14:56 -0400

    Controllers and Category picked added

---
+++ database/migrations/2020_10_01_045430_create_category_picked_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryPickedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_picked', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->integer('num_picked');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_picked');
+    }
+}
