2e47ae139b61b5cfae142e8c3b02817732e39d6b
maleke <malekelthomas97@gmail.com>
2020-10-03 14:03:16 -0400

    Updated Nav Bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,12 +50,17 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="dropdown-item" href="/userhome">Profile</a>
+                            </li>
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -65,8 +70,11 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
+
                                 </div>
+
                             </li>
+
                         @endguest
                     </ul>
                 </div>
