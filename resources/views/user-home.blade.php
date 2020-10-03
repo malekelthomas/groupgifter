92c990fb28cbf9d4ae0fd288951caaf2b91282a8
maleke <malekelthomas97@gmail.com>
2020-10-03 11:09:14 -0400

    User home and styling updated

--- resources/views/user-home.blade.php
+++ resources/views/user-home.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.app')
+<?php
 
+use Illuminate\Support\Facades\Auth;
+
+?>
 @section('content')
 
+
+<div class="w-100" style="height: 100px;"></div>
+<div class="w-100" style="height: 100px;"></div>
+<div class="wrapper">
+    <div class="row justify-content-center">
+
+        <div id="profile_picture">
+            <img src="/images/blank-profile-picture-png.png" alt="">
+        </div>
+    </div>
+    <div class="w-100" style="height: 100px;"></div>
+    <div class="row justify-content-center">
+        <div class="user-home-btn col-sm-2 mb-3">
+            <p style="margin-top: 10%"><strong>CHOOSE PRODUCTS</strong></p>
+        </div>
+        <div class="user-home-btn col-sm-2 offset-sm-1 mb-3">
+            <p style="margin-top: 10%"><strong>VIEW GROUPS</strong></p>
+        </div>
+        <div class="user-home-btn col-sm-2 offset-sm-1 mb-3">
+            <p style="margin-top: 10%"><strong>CREATE GROUP</strong></p>
+        </div>
+
+    </div>
+
+
+</div>
 @endsection
