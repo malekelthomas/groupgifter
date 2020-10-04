af2953efd99a35d1b312c405948ef96ea8d4f7dd
maleke <malekelthomas97@gmail.com>
2020-10-04 13:30:36 -0400

    Changed blade to form

--- resources/views/search-groups.blade.php
+++ resources/views/search-groups.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 
+
 @section('content')
 
-    <h1>{{$member}}</h1>
+
+<form  id="notificationForm" method="POST" action="/send-notification">
+    @csrf
+
+
+    <input type="hidden" id="{{$member}}" name="member" value="{{$member}}">
+
+
+</form>
 
 
 @endsection
