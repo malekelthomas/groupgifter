0eeb5592cb7e63e8f3f7bb0433fe23440361427d
maleke <malekelthomas97@gmail.com>
2020-10-09 01:39:20 -0400

    groups change to inputs

--- resources/views/view-groups.blade.php
+++ resources/views/view-groups.blade.php
@@ -2,9 +2,35 @@
 
 @section('content')
 
-    @foreach ($groups as $group)
-        <h1>{{$group->name}}</h1>
-    @endforeach
+
+
+
+<div class="selector">
+    <ul>
+        <?php
+
+            $i =0;
+
+            foreach($groups as $group){
+
+                echo "<li>";
+                echo "<input type='checkbox' id='c$i'>";
+                echo "<label for ='c$i' style='color:#fff'>{$group->name}</label>";
+                echo "</li>";
+                $i++;
+            }
+            ?>
+    </ul>
+
+    {{-- <div class="selector">
+        <ul>
+            <li>
+                <input type="checkbox" id="c1">
+                <label for="c1"></label>
+            </li>
+
+        </ul>
+    </div> --}}
 
 
 @endsection
