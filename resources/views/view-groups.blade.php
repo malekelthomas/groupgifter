9a07e433186503b376de482e8ad55514bcdc2c62
maleke <malekelthomas97@gmail.com>
2020-10-13 13:24:47 -0400

    Added form

--- resources/views/view-groups.blade.php
+++ resources/views/view-groups.blade.php
@@ -5,22 +5,26 @@
 
 
 
-<div class="selector">
-    <ul>
-        <?php
+<form id="userGroupsForm" method ="POST" action="/group/viewmembers">
+    @csrf
+    <div class="selector">
+        <ul>
+            <?php
 
-            $i =0;
+                $i =0;
 
-            foreach($groups as $group){
+                foreach($groups as $group){
 
-                echo "<li>";
-                echo "<input type='checkbox' id='c$i'>";
-                echo "<label for ='c$i' style='color:#fff'>{$group->name}</label>";
-                echo "</li>";
-                $i++;
-            }
-            ?>
-    </ul>
+                    echo "<li>";
+                    echo "<input onchange='this.form.submit()' type='checkbox' id='c$i' name='group' value={$group->name}>";
+                    echo "<label for ='c$i' style='color:#fff'>{$group->name}</label>";
+                    echo "</li>";
+                    $i++;
+                }
+                ?>
+        </ul>
+    </div>
+</form>
 
     {{-- <div class="selector">
         <ul>
